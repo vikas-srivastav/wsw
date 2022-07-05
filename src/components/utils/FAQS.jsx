08ad26055faa70b01c7450d5ff3e687a506c82a7
Vikas Kumar <jmbliss13@gmail.com>
2022-07-05 16:50:33 +0530

    anurag new code

--- src/components/utils/FAQS.jsx
+++ src/components/utils/FAQS.jsx
@@ -168,3 +168,14 @@ const FAQS = () => {
 }
 
 export default FAQS;
+
+
+
+
+<div className="accordion-item-faq">
+      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <div>{isActive ? 'x' : '+'}</div>
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </div>
